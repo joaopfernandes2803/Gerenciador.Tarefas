f6d1b9770c1333807c03850e6530c7e1078ebcbd
João Pedro Fernandes <joaopfernandes2803@gmail.com>
2025-10-30 21:33:57 -0300

    Novas opções
    Adicionando as opções de marca a tarefa como concluída e de remover tarefas. Foi feito a troca de aspas duplas por aspas simples.

--- SistemaGerenciador/Sistema/lista_tarefas.py
+++ SistemaGerenciador/Sistema/lista_tarefas.py
@@ -9,17 +9,35 @@ class Lista:
 
     def listar_tarefas(self):
         if not self.lista:
-            print("Nenhuma tarefa adicionada ainda.")
+            print('Nenhuma tarefa adicionada ainda.')
         else:
-            print("\nSuas tarefas:")
+            print('\nSuas tarefas:')
             for i, tarefa in enumerate(self.lista, start=1):
-                print(f"{i}. {tarefa}")
+                print(f'{i}. {tarefa}')
 
     def marca_tarefa(self):
-        pass
+        if not self.lista:
+            print('Nenhuma tarefa adicionada ainda.')
+        else:
+            print('\nSuas tarefas:')
+            for i, tarefa in enumerate(self.lista, start=1):
+                print(f'{i}. {tarefa}')
+
+        marcar = int(input('Digite o número da tarefa a marcar como concluída\n-> '))
+        self.lista[marcar-1] = f'[✅]{self.lista[marcar-1]}'
+        print(f'Tarefa{self.lista[marcar-1]}  marcada como concluída.')
 
     def remover_tarefa(self):
-        pass
+        if not self.lista:
+            print('Nenhuma tarefa adicionada ainda.')
+        else:
+            print('\nSuas tarefas:')
+            for i, tarefa in enumerate(self.lista, start=1):
+                print(f'{i}. {tarefa}')
+        remover = int(input('Digite o número da tarefa a ser removida\n-> '))
+        if 1<= remover <= len(self.lista):
+            tarefa_removida = self.lista.pop(remover-1)
+            print(f'{tarefa_removida} foi removido da lista.')
 
     def salvar_carregar_tarefas(self):
         pass
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from SistemaGerenciador.Sistema.lista_tarefas import Lista
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sistema = Lista()
 
     while True:
-        print("\n1 - Adicionar tarefa")
-        print("2 - Listar tarefas")
-        print("3 - Sair")
+        print('-'*30)
+        print('GERENCIADOR DE TAREFAS'.center(30))
+        print('-'*30)
+        print('\n1 - Adicionar tarefa')
+        print('2 - Listar tarefas')
+        print('3 - Marca tarefa')
+        print(f'4 - Remover tarefa')
+        print('6 - Sair')
 
-        opcao = input("Escolha: ")
+        opcao = input('Escolha: ')
 
-        if opcao == "1":
+        if opcao == '1':
             sistema.adicionar_tarefas()
-        elif opcao == "2":
+        elif opcao == '2':
             sistema.listar_tarefas()
-        elif opcao == "3":
-            print("Encerrando o sistema...")
+        elif opcao == '3':
+            sistema.marca_tarefa()
+        elif opcao == '4':
+            sistema.remover_tarefa()
+        elif opcao == '6':
+            print('Encerrando o sistema...')
             break
         else:
-            print("Opção inválida!")
+            print('Opção inválida!')
