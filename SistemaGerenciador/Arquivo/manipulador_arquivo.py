779394405566b3deba0f7058ca26a403bcd06b3f
João Pedro Fernandes <joaopfernandes2803@gmail.com>
2025-11-03 20:06:10 -0300

    Adicionando csv
    Adiciona suporte a salvar e carregar tarefas em formato CSV com Pandas

--- SistemaGerenciador/Arquivo/manipulador_arquivo.py
+++ SistemaGerenciador/Arquivo/manipulador_arquivo.py
@@ -1,13 +1,14 @@
-def salvar_tarefas(self,arquivo = 'tarefas.txt'):
-    with open(arquivo,'w', encoding='utf-8') as arq:
-        for tarefa in self.lista:
-            arq.write(tarefa +'\n')
+import pandas as pd
+
+def salvar_tarefas(self, arquivo = 'tarefas.csv'):
+    df = pd.DataFrame(self.lista, columns=['Tarefa'])
+    df.to_csv(arquivo, index=False)
     print('Tarefas salvas com sucesso!')
 
-def carregar_tarefas(self,arquivo = 'tarefas.txt'):
+def carregar_tarefas(self,arquivo = 'tarefas.csv'):
     try:
-        with open(arquivo,'r', encoding = 'utf-8') as arquivo:
-            self.lista = [linha.strip() for linha in arquivo.readlines()]
+        df = pd.read_csv(arquivo)
+        self.lista = df['Tarefa'].tolist()
         print(f'{len(self.lista)} tarefas carregadas com sucesso!')
     except FileNotFoundError:
         print('Nenhum arquivo de tarefas encontrado. Um novo será criado ao salvar.')
